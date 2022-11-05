8df23c8e947b10c72f9b26cd254660a4cabeb50b
LuisVillatoro02 <lvillatorom4@miumg.edu.gt>
2022-11-05 09:24:18 -0600

    Se crean las migraciones e Instalacion de Vue

---
+++ database/migrations/2022_11_05_152112_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo_producto');
+            $table->string('nombre');
+            $table->string('descipcion');
+            $table->string('id_marca');
+            $table->string('imagen');
+            $table->string('fecha_vencimiento');
+            $table->string('hora_registro');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
